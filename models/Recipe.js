b01a57518b5e3ea6e7ff69cdd4c2f06ffa4adde2
IliesBourib <bourib@mediamarktsaturn.com>
2019-09-29 17:57:08 +0200

    lab finished

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type:String},
+  level: {type: String, enum:['Easy Peasy','Amateur Chef','UltraPro Chef']},
+  ingredients: {type:Array},
+  cuisine: {type:String,required:true},
+  dishType: {type: String,value: ['Breakfast','Dish','Snack','Drink','Dessert','Other']},
+  image: {type: String,default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type:String},
+  creator: {type:String},
+  created:{type:Date,default:Date.now}
+
+  
 });
+
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
